4abd0f2dc8c55fa1fd93b8c25fa3f69473761cb5
Wiktor Koziak <headelzon@users.noreply.github.com>
2017-03-04 21:10:52 +0100

    ABBA rhymes and blank verse added

--- poem.py
+++ poem.py
@@ -4,7 +4,7 @@
 import rhymes
 
 
-def is_valid(prompt):
+def is_valid(prompt, low, high):
     while True:
         try:
             value = int(input(prompt))
@@ -12,7 +12,7 @@ def is_valid(prompt):
             print('Use a number. Try again!')
             continue
 
-        if value < 1 or value > 5:
+        if value < low or value > high:
             print('Use a number between 1 and 5. Try again!')
             continue
         else:
@@ -28,10 +28,10 @@ def menu():
     print('   4) Other')
     print('    5) Exit \n')
 
+
 # def save():
 # instead of printing lines in rhymes.py append them to list poem[] and then print
 # to save, just write in consecutive entities of the list poem[]
-
 
 def main():
     loop = True
@@ -39,24 +39,39 @@ def main():
 
     while loop:
         menu()
-        choice = is_valid('Your choice: ')
+        choice = is_valid('Your choice: ', 1, 5)
 
         if choice == 1:
-            print('AABB')
-            verses = int(input('Please input number of stanzas (verses) 1-9: '))
+            verses = is_valid('Please input number of stanzas (verses) 1-9: ', 1, 9)
             print('')
             rhymes.aabb(verses)
         elif choice == 2:
-            print('ABBA')
-            # ABBA
+            verses = is_valid('Please input number of stanzas (verses) 1-9: ', 1, 9)
+            print('')
+            rhymes.abba(verses)
         elif choice == 3:
-            print('ABAB')
-            verses = int(input('Please input number of stanzas (verses) 1-9: '))
+            verses = is_valid('Please input number of stanzas (verses) 1-9: ', 1, 9)
             print('')
             rhymes.abab(verses)
         elif choice == 4:
-            print('Other')
-            # Other
+            print('\tChoose from below:')
+            print('\t1) A blank verse (no rhymes)')
+            print('\t 2) A sonnet (2 stanzas with 4 lines and 2 stanzas with 3 lines')
+            print('\t  3) Back\n')
+            choice2 = is_valid('Your choice: ', 1, 3)
+
+            while True:
+                if choice2 == 1:
+                    verses = is_valid('Please input number of stanzas (verses) 1-20: ', 1, 20)
+                    print('')
+                    rhymes.blank(verses)
+                    break
+                elif choice2 == 2:
+                    print('Sonnet under construction!')
+                    break
+                else:
+                    break
+
         elif choice == 5:
             print('Bye!')
             loop = False
--- rhymes.py
+++ rhymes.py
@@ -52,7 +52,7 @@ def aabb(verse):
 
     for x in range(verse):
         while True:
-            line = random.randrange(how_long) # random numbers generation
+            line = random.randrange(how_long)  # random numbers generation
             if line in numbers:
                 continue
             else:
@@ -65,8 +65,8 @@ def aabb(verse):
 
     for x in range(verse):
         if x % 4 == 0:
-            print(a[numbers[x]]) # print random (from list numbers[]) element of list a or b
-        elif (x-1) % 4 == 0:
+            print(a[numbers[x]])  # print random (from list numbers[]) element of list a or b
+        elif (x - 1) % 4 == 0:
             print(a[numbers[x]])
         elif (x % 2 == 0) and (x % 4 != 0):
             print(b[numbers[x]])
@@ -74,6 +74,109 @@ def aabb(verse):
             print(b[numbers[x]])
 
 
-# ABBA
-# For x divisible by 3 (0, 3, 6, 9, ...) - rhyma A
-# For other - rhyme B
+def abba(verse):
+    f = open('a.txt', 'r')
+    g = open('b.txt', 'r')
+
+    a, b, numbers = [], [], []
+
+    for line in f:
+        line2 = line.replace("\n", "")  # Delete newline at the end of each line
+        a.append(line2)
+
+    for line in g:
+        line2 = line.replace("\n", "")
+        b.append(line2)
+
+    how_long = min(len(a), len(b))
+
+    for x in range(verse):
+        while True:
+            line = random.randrange(how_long)  # random numbers generation
+            if line in numbers:
+                continue
+            else:
+                numbers.append(line)
+                break
+
+    # ABBA
+    # For x divisible by 3 (0, 3, 6, 9, ...) - rhyma A
+    # For other - rhyme B
+
+    for x in range(verse):
+        if x % 3 == 0:
+            print(a[numbers[x]])
+        else:
+            print(b[numbers[x]])
+
+
+# def sonnet():
+#     f = open('a.txt', 'r')
+#     g = open('b.txt', 'r')
+#     h = open('c.txt', 'r')
+#     i = open('d.txt', 'r')
+#
+#     a, b, c, d, numbers_a, numbers_b = [], [], [], [], [], []
+#
+#     for line in f:
+#         line2 = line.replace("\n", "")  # Delete newline at the end of each line
+#         a.append(line2)  # Append to list a[]
+#
+#     for line in g:
+#         line2 = line.replace("\n", "")
+#         b.append(line2)
+#
+#     for line in h:
+#         line2 = line.replace("\n", "")
+#         c.append(line2)
+#
+#     for line in i:
+#         line2 = line.replace("\n", "")
+#         d.append(line2)
+#
+#     how_long_a = min(len(a), len(b))
+#     how_long_b = min(len(c), len(d))
+#
+#     for x in range(14):
+#         while True:
+#             line_a = random.randrange(how_long_a)  # random numbers generation
+#             line_b = random.randrange(how_long_b)
+#             if line_a in numbers_a:
+#                 continue
+#             elif line_b in numbers_b:
+#                 continue
+#             else:
+#                 numbers_a.append(line_a)
+#                 numbers_b.append(line_b)
+#
+#     for x in range(14):
+#         if x in [0, 3, 4, 7]:
+#             print(a[numbers_a[x]])
+#         elif x in [1, 2, 5, 6]:
+#             print(b[numbers_a[x]])
+#         elif x in [8, 10, 12]:
+#             print(c[numbers_b[x]])
+#         else:
+#             print(d[numbers_b[x]])
+
+
+def blank(verse):
+    f = open('blank.txt', 'r')
+
+    a, numbers = [], []
+
+    for line in f:
+        line2 = line.replace("\n", "")  # Delete newline at the end of each line
+        a.append(line2)  # Append to list a[]
+
+    for x in range(verse):
+        while True:
+            line = random.randrange(len(a))  # random numbers generation
+            if line in numbers:
+                continue
+            else:
+                numbers.append(line)
+                break
+
+    for x in range(verse):
+        print(a[numbers[x]])
